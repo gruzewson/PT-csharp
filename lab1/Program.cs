139e4007d5fefaa033317340589fc35d208c4a4d
gruzewson <gruzewskimarcel@gmail.com>
2024-04-17 23:13:32 +0200

    Initializing new repo

---
+++ lab1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lab1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
