85dfc358b0b05aeef278a02d93516aa785a4c259
gruzewson <gruzewskimarcel@gmail.com>
2024-04-17 23:31:44 +0200

    added first task

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace lab1
+﻿using System;
+using System.IO;
+
+namespace lab1
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No arguements");
+                return;
+            }
+
+            string directoryPath = args[0];
+
+            if (!Directory.Exists(directoryPath))
+            {
+                Console.WriteLine("Given catalog doesn't exist");
+                return;
+            }
+
+            Console.WriteLine($"Catalog contents: {directoryPath}");
+            
+            string[] files = Directory.GetFiles(directoryPath);
+            string[] directories = Directory.GetDirectories(directoryPath);
+
+            Console.WriteLine("Files:");
+            foreach (string file in files)
+            {
+                Console.WriteLine("\t" + Path.GetFileName(file));
+            }
+
+            Console.WriteLine("Subcatalogs:");
+            foreach (string dir in directories)
+            {
+                Console.WriteLine("\t" + Path.GetFileName(dir));
+            }
         }
     }
 }
