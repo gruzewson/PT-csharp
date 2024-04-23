ac632479f625b1e9361b3d7af97cee3f6406e6bb
gruzewson <gruzewskimarcel@gmail.com>
2024-04-23 17:21:45 +0200

    added DOS attributes and sizes

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,33 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace lab1
 {
     internal class Program
     {
-        public static void PrintContents(string directoryPath, string tabs)
-        {
-            try
-            {
-                string[] files = Directory.GetFiles(directoryPath);
-                string[] directories = Directory.GetDirectories(directoryPath);
-                tabs += "  ";
-        
-                foreach(string file in files)
-                {
-                    Console.WriteLine(tabs + Path.GetFileName(file));
-                }
-                foreach (string dir in directories)
-                {
-                    Console.WriteLine(tabs + Path.GetFileName(dir));
-                    PrintContents(directoryPath + "\\" + Path.GetFileName(dir), tabs);
-                }
-            }
-            catch (DirectoryNotFoundException ex)
-            {
-                Console.WriteLine($"Directory not found: {ex.Message}");
-            }
-        }
         public static void Main(string[] args)
         {
             if (args.Length == 0)
@@ -37,6 +16,7 @@ namespace lab1
             }
 
             string directoryPath = args[0];
+            DirectoryInfo directory = new DirectoryInfo(directoryPath);
 
             if (!Directory.Exists(directoryPath))
             {
@@ -45,7 +25,62 @@ namespace lab1
             }
 
             Console.WriteLine($"\nCatalog '{directoryPath}' contents:");
-            PrintContents(directoryPath, "");
+            DateTime oldestDate = PrintContentsAndFindOldest(directoryPath, "");
+            Console.WriteLine($"\nOldest file: {oldestDate}\n");
         }
+        
+        static DateTime PrintContentsAndFindOldest(string directoryPath, string tabs)
+        {
+            DateTime oldestDate = DateTime.MaxValue;
+            try
+            {
+                string[] files = Directory.GetFiles(directoryPath);
+                string[] directories = Directory.GetDirectories(directoryPath);
+                tabs += "  ";
+
+                foreach (string file in files)
+                {
+                    FileSystemInfo info = new FileInfo(file);
+                    FileInfo fi = new FileInfo(file);
+                    Console.WriteLine($"{tabs} {Path.GetFileName(file)} {fi.Length} bytes {GetAttributes(info)}");
+                    DateTime fileCreatedDate = File.GetCreationTime(file);
+                    oldestDate = oldestDate < fileCreatedDate ? oldestDate : fileCreatedDate;
+                }
+                foreach (string dir in directories)
+                {
+                    int size = Directory.GetDirectories(dir).Length + Directory.GetFiles(dir).Length;
+                    Console.WriteLine($"{tabs} {Path.GetFileName(dir)} ({size}) ----");
+                    DateTime newDate = PrintContentsAndFindOldest(Path.Combine(directoryPath, Path.GetFileName(dir)), tabs); //Path.combine obsługuje znaki separatorów ścieżek
+                    oldestDate = oldestDate < newDate ? oldestDate : newDate;
+                }
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine($"Directory not found: {ex.Message}");
+            }
+            return oldestDate;
+        }
+        static string GetAttributes(FileSystemInfo info)
+        {
+            string attributes = "";
+            if ((info.Attributes & FileAttributes.ReadOnly) == FileAttributes.ReadOnly)
+                attributes += "r";
+            else
+                attributes += "-";
+            if ((info.Attributes & FileAttributes.Archive) == FileAttributes.Archive)
+                attributes += "a";
+            else
+                attributes += "-";
+            if ((info.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden)
+                attributes += "h";
+            else
+                attributes += "-";
+            if ((info.Attributes & FileAttributes.System) == FileAttributes.System)
+                attributes += "s";
+            else
+                attributes += "-";
+            return attributes;
+        } 
+        
     }
 }
