163aa94eeec3174e89c81d19c286c1a80d5ddab0
gruzewson <gruzewskimarcel@gmail.com>
2024-05-07 20:33:29 +0200

    added tree view and deleting

--- lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
+++ lab1/obj/Debug/net8.0/lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+378382c9ece78be1b42723d70c7ffb9e4abb7087")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b443bdea7e758d526f5624c55fda5a0b0538fcb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
